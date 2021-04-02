e12b02777134914277efdae2f3a33ab0f96fbc5b
Roman <77735722+RozhokR@users.noreply.github.com>
2021-04-02 17:12:06 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const menuSlide = () => {
+  const burger = document.querySelector('.mobile-nav');
+  const header = document.querySelector('.header');
+
+  burger.addEventListener('click', () => {
+    header.classList.toggle('header--opened');
+    header.classList.toggle('header--closed');
+    burger.classList.toggle('mobile-nav--opened');
+  });
+};
+
+menuSlide();
